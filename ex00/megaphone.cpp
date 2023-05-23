34c9479e98d9ac5c94f268cc59b93b8f6bf081d4
Lucas De Alencastro Moreira <85504976+lde-alen@users.noreply.github.com>
2023-05-23 09:00:04 +0000

    done with ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+
+bool isAlphaNumeric(const std::string &str)
+{
+    auto it = std::find_if(str.begin(), str.end(), [](char const &c) {
+            return !isalnum(c);
+        });
+ 
+    return it == str.end();
+}
 
 void shout(std::string str)
 {
     for(int x = 0; x < str.length(); x++)
         std::cout << char(toupper(str[x]));
+        std::cout << '\n';
 }
 
 int main(int ac, char **av)
 {
     int ret;
+    int i;
+    std::string str;
 
+    i = 1;
     ret = 0;
     if (ac == 1)
     {
@@ -18,6 +32,17 @@ int main(int ac, char **av)
         return (++ret);
     }
     else
-        shout(av[1]);
+    {
+        while (av[i])
+        {
+            if (isAlphaNumeric(av[i]) && i != 1)
+                str.append(", ");
+            else
+                str.append(" ");
+            str.append(av[i]);
+            i++;
+        }
+        shout(str);
+    }
     return (ret);
 }
